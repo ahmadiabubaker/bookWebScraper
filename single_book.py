d31ba62db9065e376a9ec43badf6584ed7160907
Abubaker <abubakerahmadi45@gmail.com>
2026-07-28 14:24:27 -0400

    Add single book web scraper with CSV export

--- single_book.py
+++ single_book.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import csv
 
 url = "https://books.toscrape.com/" 
 
@@ -15,22 +16,21 @@ link = url + book.find("a")["href"]
 
 book_response = requests.get(link)
 book_soup = BeautifulSoup(book_response.content, "html.parser")
-category = book_soup.find("ul", class_="breadcrumb").find_all("li")[2].string
+category = book_soup.find("ul", class_="breadcrumb").find_all("li")[2].find("a").string
 description = book_soup.find("div", id="product_description").find_next_sibling("p").string
 table = book_soup.find("table", class_="table-striped")
+img_src = url  + book_soup.find("div", class_="item active").find("img")["src"]
+image_url = img_src.replace("../../", "")
+
 rows = table.find_all("td")
 upc = rows[0].string
 price_excl_tax = rows[2].string
 price_incl_tax = rows[3].string
 quantity_available = rows[5].string
-print(upc)
-print(price_excl_tax)
-print(price_incl_tax)
-print(quantity_available)
-print(category)
-print(description)
 
-print(price)
-print(title)
-print(rating)
-print(link)
+
+with open("book_data.csv", "w", newline="", encoding="utf-8") as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerow(["product_page_url", "universal_product_code", "book_title", "price_including_tax", "price_excluding_tax", "quantity_available", "product_description", "category", "review_rating", "image_url"])
+    writer.writerow([link, upc, title, price_incl_tax, price_excl_tax, quantity_available, description, category, rating, image_url])
+
